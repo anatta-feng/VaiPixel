d99ad62ef5684cc7dccd1d8a2070e1f03617c99a
emrysf <1@fxcdev.com>
2019-06-27 13:25:58 +0800

    fix bug

--- backend/src/main/java/com/emrys/vaipixel/db/mapper/WorkMapper.java
+++ backend/src/main/java/com/emrys/vaipixel/db/mapper/WorkMapper.java
@@ -1,5 +1,8 @@
 package com.emrys.vaipixel.db.mapper;
 
+import com.emrys.vaipixel.db.enums.WorkTypeEnum;
+import com.emrys.vaipixel.db.model.Photo;
+import com.emrys.vaipixel.db.model.Video;
 import com.emrys.vaipixel.db.model.Work;
 import com.emrys.vaipixel.dto.cond.WorkCond;
 import org.apache.ibatis.annotations.*;
@@ -14,11 +17,35 @@ public interface WorkMapper {
             "FROM t_work AS work " +
             "LEFT JOIN t_photo ON t_photo.work_id = work.work_id " +
             "LEFT JOIN t_video ON t_video.work_id = work.work_id ")
-    @ResultMap("com.emrys.vaipixel.db.mapper.WorkMapper.WorkResultMap")
+    @Results({
+            @Result(column = "author_id", property = "author", one = @One(select = "com.emrys.vaipixel.db.mapper.UserMapper.getUserById")),
+            @Result(column = "category_id", property = "category", one = @One(select = "com.emrys.vaipixel.db.mapper.CategoryMapper.getCategoryById")),
+            @Result(column = "work_id", property = "tags", many = @Many(select = "com.emrys.vaipixel.db.mapper.TagMapper.getWorkTags")),
+            @Result(column = "work_id", property = "workId"),
+            @Result(column = "large_url", property = "image.largeUrl"),
+            @Result(column = "medium_url", property = "image.mediumUrl"),
+            @Result(column = "small_url", property = "image.smallUrl"),
+            @Result(column = "thumb_url", property = "image.thumbUrl"),
+    })
+    @TypeDiscriminator(javaType = WorkTypeEnum.class, column = "type", cases = {
+            @Case(value = "photo", type = Photo.class, results = {
+                    @Result(column = "camera", property = "cameraParameter.camera"),
+                    @Result(column = "lens", property = "cameraParameter.lens"),
+                    @Result(column = "focal_length", property = "cameraParameter.focalLength"),
+                    @Result(column = "aperture", property = "cameraParameter.aperture"),
+                    @Result(column = "iso", property = "cameraParameter.iso"),
+                    @Result(column = "shutter", property = "cameraParameter.shutter"),
+            }),
+            @Case(value = "video", type = Video.class, results = {
+                    @Result(column = "device_name", property = "deviceName"),
+                    @Result(column = "play_url", property = "playUrl")
+            })
+    })
     List<Work> getWorks();
 
     @Select("SELECT work.work_id, title, description, created_time, updated_time, width, height, type, large_url, " +
-            "medium_url, small_url, thumb_url, author_id, category_id, like_count, page_view, user_view " +
+            "medium_url, small_url, thumb_url, author_id, category_id, like_count, page_view, user_view, camera, " +
+            "lens, focal_length, aperture, iso, shutter, device_name, play_url " +
             "FROM t_work AS work " +
             "LEFT JOIN t_photo ON t_photo.work_id = work.work_id " +
             "LEFT JOIN t_video ON t_video.work_id = work.work_id " +

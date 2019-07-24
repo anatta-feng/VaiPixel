f0438b4acddb1f827a90fd5034bbe84dd8552ee2
emrysf <1@fxcdev.com>
2019-07-24 10:33:23 +0800

    Update upload

--- frontend/src/components/upload/UploadBtn.vue
+++ frontend/src/components/upload/UploadBtn.vue
@@ -1,30 +1,41 @@
 <template>
-  <div class="" @click="upload">
-    <img src="../../assets/img/upload/icon_upload.png" alt="Upload">
-    <div>Drop your image here or <span>Browser</span></div>
-    <qiniu-img-upload
-    ref="qiniuImgUpload"></qiniu-img-upload>
-
+  <div class @click="upload">
+    <img src="../../assets/img/upload/icon_upload.png" alt="Upload" />
+    <div>
+      Drop your image here or
+      <span>Browser</span>
+    </div>
+    <qiniu-img-upload ref="qiniuImgUpload" :token="props.token"></qiniu-img-upload>
   </div>
 </template>
 
 <script>
-  import QiniuImgUpload from './qiniu-img-upload'
+import QiniuImgUpload from "./qiniu-img-upload";
 
-  export default {
-    name: 'UploadBtn',
-    components: { QiniuImgUpload },
-    methods: {
-      upload () {
-        this.$refs.qiniuImgUpload.$el.dispatchEvent(new MouseEvent('click'))
-      },
-      addFile ({ target: { files } }) {
-        console.log(files)
-      }
+export default {
+  name: "UploadBtn",
+  props: {
+    token : {
+      type:String,
+      default :'adcasc'
+    }
+  },
+  components: { QiniuImgUpload },
+  data() {
+    return {
+
+    }
+  },
+  methods: {
+    upload() {
+      this.$refs.qiniuImgUpload.$el.dispatchEvent(new MouseEvent("click"));
+    },
+    addFile({ target: { files } }) {
+      console.log(files);
     }
   }
+};
 </script>
 
 <style scoped>
-
 </style>
--- frontend/src/components/upload/qiniu-img-upload.vue
+++ frontend/src/components/upload/qiniu-img-upload.vue
@@ -25,33 +25,29 @@ export default {
     onProgress: Function,
     onSuccess: Function,
     onFailed: Function,
-    onFinished: Function
   },
   methods: {
     addFile({ target: { files } }) {
+      console.log(this.token)
       let tempFile = files[0];
       tempFile.url = URL.createObjectURL(tempFile);
       this.file = tempFile;
-      // this.onChange(tempFile)
-      this.submit()
+      this.onChange(tempFile)
+      this.submit();
     },
     remove() {
       this.file = null;
-      // this.onChange(null)
+      this.onChange(null)
     },
     submit() {
-      console.log("asda");
       if (this.checkIfCanUpload()) {
-      
-        let token =
-          "brk4iEsr7VV17iirO4ls2fd4aRJh7viW-kHCCYVO:mNBfZUSWw2Y8INts7JmhB3pHEzc=:eyJjYWxsYmFja0JvZHlUeXBlIjoiYXBwbGljYXRpb24vanNvbiIsInNjb3BlIjoidmFpcGl4ZWwiLCJyZXR1cm5Cb2R5Ijoie1wia2V5XCI6XCIkKGtleSlcIixcImhhc2hcIjpcIiQoZXRhZylcIixcImJ1Y2tldFwiOlwiJChidWNrZXQpXCIsXCJmc2l6ZVwiOiQoZnNpemUpLFwiZXhpZlwiOiR7ZXhpZn19IiwiY2FsbGJhY2tVcmwiOiJodHRwOi8vcmVxdWVzdGJpbi5mdWxsY29udGFjdC5jb20vMWd3ZTY0NDEiLCJkZWFkbGluZSI6MTU2Mzg5NzU0MywiY2FsbGJhY2tCb2R5Ijoie1wia2V5XCI6XCIkKGtleSlcIixcImhhc2hcIjpcIiQoZXRhZylcIixcImJ1Y2tldFwiOlwiJChidWNrZXQpXCIsXCJmc2l6ZVwiOiQoZnNpemUpLFwiZXhpZlwiOiR7ZXhpZn19In0=";
-        this.upload2Qiniu(this.file, token)
+        this.upload2Qiniu(this.file, this.token);
       }
     },
     checkIfCanUpload() {
       return this.file === null
         ? false
-        : (this.onBefore && this.onBefore()) || !this.onBefore;
+        : (this.onBefore && this.onBefore(this.file)) || !this.onBefore;
     },
     upload2Qiniu(file, token) {
       let key = file.name;
@@ -65,23 +61,15 @@ export default {
       };
       let observe = {
         next(res) {
-          console.log("已上传大小，单位为字节：" + res.total.loaded);
-          console.log("本次上传的总量控制信息，单位为字节：" + res.total.size);
-          console.log("当前上传进度，范围：0～100：" + res.total.percent);
+          this.onProgress(res.total.percent)
         },
         error(err) {
-          console.log(err.code);
-          console.log(err.message);
-          console.log(err.isRequestError);
-          console.log(err.reqId);
+          this.onFailed(err.code, err.message)
         },
         complete(res) {
-          //完成后的操作
-          //上传成功以后会返回key 和 hash  key就是文件名了！
-          console.log(res);
+          this.onSuccess(res)
         }
       };
-      //开始上传  token 需要找后端获取(单独的方法)
       let observable = qiniu.upload(file, key, token, putExtra, config);
       let subscription = observable.subscribe(observe);
     }

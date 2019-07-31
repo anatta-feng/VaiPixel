2d97e0f5d774256e661cf37e4983f3b031370bca
emrysf <1@fxcdev.com>
2019-08-01 00:39:25 +0800

    add value for token

--- frontend/src/components/upload/UploadBtn.vue
+++ frontend/src/components/upload/UploadBtn.vue
@@ -1,40 +1,40 @@
 <template>
   <div class @click="upload">
-    <img src="../../assets/img/upload/icon_upload.png" alt="Upload" />
+    <img src="../../assets/img/upload/icon_upload.png" alt="Upload"/>
     <div>
       Drop your image here or
       <span>Browser</span>
     </div>
-    <qiniu-img-upload ref="qiniuImgUpload" :token="props.token"></qiniu-img-upload>
+    <qiniu-img-upload ref="qiniuImgUpload" :token="qiniuToken"></qiniu-img-upload>
   </div>
 </template>
 
 <script>
-import QiniuImgUpload from "./qiniu-img-upload";
+import QiniuImgUpload from './qiniu-img-upload'
 
 export default {
-  name: "UploadBtn",
+  name: 'UploadBtn',
   props: {
-    token : {
-      type:String,
-      default :'adcasc'
+    token: {
+      type: String,
+      default: 'adada'
     }
   },
   components: { QiniuImgUpload },
-  data() {
+  data () {
     return {
-
+      qiniuToken: this.token
     }
   },
   methods: {
-    upload() {
-      this.$refs.qiniuImgUpload.$el.dispatchEvent(new MouseEvent("click"));
+    upload () {
+      this.$refs.qiniuImgUpload.$el.dispatchEvent(new MouseEvent('click'))
     },
-    addFile({ target: { files } }) {
-      console.log(files);
+    addFile ({ target: { files } }) {
+      console.log(files)
     }
   }
-};
+}
 </script>
 
 <style scoped>
--- frontend/src/components/upload/qiniu-img-upload.vue
+++ frontend/src/components/upload/qiniu-img-upload.vue
@@ -12,10 +12,10 @@
 </template>
 
 <script>
-import * as qiniu from "qiniu-js";
+import * as qiniu from 'qiniu-js'
 
 export default {
-  name: "qiniu-img-upload",
+  name: 'qiniu-img-upload',
   props: {
     multiple: Boolean,
     token: String,
@@ -24,57 +24,62 @@ export default {
     onBefore: Function,
     onProgress: Function,
     onSuccess: Function,
-    onFailed: Function,
+    onFailed: Function
+  },
+  data () {
+    return {
+      submitFile: this.file
+    }
   },
   methods: {
-    addFile({ target: { files } }) {
+    addFile ({ target: { files } }) {
       console.log(this.token)
-      let tempFile = files[0];
-      tempFile.url = URL.createObjectURL(tempFile);
-      this.file = tempFile;
+      let tempFile = files[0]
+      tempFile.url = URL.createObjectURL(tempFile)
+      this.submitFile = tempFile
       this.onChange(tempFile)
-      this.submit();
+      this.submit()
     },
-    remove() {
-      this.file = null;
+    remove () {
+      this.submitFile = null
       this.onChange(null)
     },
-    submit() {
+    submit () {
       if (this.checkIfCanUpload()) {
-        this.upload2Qiniu(this.file, this.token);
+        this.upload2Qiniu(this.submitFile, this.token)
       }
     },
-    checkIfCanUpload() {
-      return this.file === null
+    checkIfCanUpload () {
+      return this.submitFile === null
         ? false
-        : (this.onBefore && this.onBefore(this.file)) || !this.onBefore;
+        : (this.onBefore && this.onBefore(this.submitFile)) || !this.onBefore
     },
-    upload2Qiniu(file, token) {
-      let key = file.name;
+    upload2Qiniu (file, token) {
+      let key = file.name
       let config = {
         useCdnDomain: true
-      };
+      }
       let putExtra = {
         fname: file,
         params: {},
-        mimeType: ["image/png", "image/jpeg", "image/jpg"]
-      };
+        mimeType: ['image/png', 'image/jpeg', 'image/jpg']
+      }
       let observe = {
-        next(res) {
+        next (res) {
           this.onProgress(res.total.percent)
         },
-        error(err) {
+        error (err) {
           this.onFailed(err.code, err.message)
         },
-        complete(res) {
+        complete (res) {
           this.onSuccess(res)
         }
-      };
-      let observable = qiniu.upload(file, key, token, putExtra, config);
-      let subscription = observable.subscribe(observe);
+      }
+      let observable = qiniu.upload(file, key, token, putExtra, config)
+      let subscription = observable.subscribe(observe)
     }
   }
-};
+}
 </script>
 
 <style scoped>
